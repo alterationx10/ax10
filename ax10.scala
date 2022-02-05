6ffbfb071fac4eb335c90845ae4139209a73c424
Mark Rudolph <mark@k8ty.app>
2022-02-05 11:27:12 -0500

    code clean up + formatting

--- ax10.scala
+++ ax10.scala
@@ -15,12 +15,13 @@ trait Hasher {
   def validate(message: String, key: String, hash: String): Task[Boolean]
 }
 
-case class MacHasher(mac: Mac) extends Hasher {
+// The live, default implementation of our Hasher Service.
+case class HasherLive(mac: Mac) extends Hasher {
 
   override def hash(message: String, key: String): Task[String] =
     for {
-      hash <- ZIO.attempt(mac.doFinal(message.getBytes("UTF-8")))
-      encoded <- B64.base64Encode(hash)
+      hash    <- ZIO.attempt(mac.doFinal(message.getBytes("UTF-8")))
+      encoded <- HashHelper.base64Encode(hash)
     } yield encoded
 
   override def validate(
@@ -29,12 +30,13 @@ case class MacHasher(mac: Mac) extends Hasher {
       msgHash: String
   ): Task[Boolean] =
     for {
-      hash <- ZIO.attempt(mac.doFinal(message.getBytes("UTF-8")))
-      encoded <- B64.base64Encode(hash)
+      hash    <- ZIO.attempt(mac.doFinal(message.getBytes("UTF-8")))
+      encoded <- HashHelper.base64Encode(hash)
     } yield encoded == msgHash
 
 }
 
+// Companion object with accessors
 object Hasher {
 
   def hash(message: String, key: String): RIO[Hasher, String] =
@@ -51,18 +53,20 @@ object Hasher {
     ZIO.effect(new SecretKeySpec(key.getBytes("UTF-8"), "HmacSHA512")).toLayer
   }
 
-  val layer: URLayer[Mac, Hasher] = (MacHasher(_)).toLayer
+  // Reference implementation layer
+  val layer: URLayer[Mac, Hasher] = (HasherLive(_)).toLayer
 
 }
 
+// Not everything needs to be/fit a Service Module pattern
 object HashHelper {
 
   def hmac512: ZLayer[SecretKeySpec, Throwable, Mac] = {
     (
       for {
-        mac <- ZIO.effect(Mac.getInstance("HmacSHA512"))
+        mac     <- ZIO.effect(Mac.getInstance("HmacSHA512"))
         keySpec <- ZIO.service[SecretKeySpec]
-        _ <- ZIO.effect(mac.init(keySpec))
+        _       <- ZIO.effect(mac.init(keySpec))
       } yield mac
     ).toLayer
   }
@@ -71,48 +75,43 @@ object HashHelper {
     ZIO.effect(new SecretKeySpec(key.getBytes("UTF-8"), "HmacSHA512")).toLayer
   }
 
-}
-
-object B64 {
   def base64Encode(bytes: Array[Byte]): Task[String] =
     ZIO.attempt(Base64.getUrlEncoder.withoutPadding().encodeToString(bytes))
 
-  def base64Encode(str: String): Task[String] =
-    ZIO.attempt(
-      Base64.getUrlEncoder
-        .withoutPadding()
-        .encodeToString(str.getBytes("UTF-8"))
-    )
-
-  def base64Decode(str: String): Task[String] =
-    ZIO.attempt(new String(Base64.getUrlDecoder.decode(str)))
-
-  def base64DecodeToBytes(str: String): Task[Array[Byte]] =
-    ZIO.attempt(Base64.getUrlDecoder.decode(str))
 }
 
 object HashApp extends ZIOAppDefault {
 
   val superSecretKey: String = "abc123"
 
+  // The overall flow of our program
   val program: ZIO[ZIOAppArgs & (Hasher & Console), Throwable, ExitCode] = for {
+    // Read the arguments
     args <- ZIOAppArgs.getArgs
-    _ <- ZIO.cond(
-      args.size == 1 || args.size == 2,
-      (),
-      new Exception("This app requires one argument to hash, and 2 to validate")
-    )
-    _ <- ZIO.when(args.size == 1) {
-      Hasher.hash(args.head, superSecretKey).flatMap(h => printLine(h))
-    }
-    _ <- ZIO.when(args.size == 2) {
-      ZIO.ifM(Hasher.validate(args.head, superSecretKey, args.last))(
-        onTrue = printLine("valid"),
-        onFalse = printLine("invalid")
-      )
-    }
+    // Make sure we've been passed only 1 or 2 args
+    _    <- ZIO.cond(
+              args.size == 1 || args.size == 2,
+              (),
+              new Exception(
+                "This app requires one argument to hash, and 2 to validate"
+              )
+            )
+    // When we've been passed 1 arg, hash it
+    _    <- ZIO.when(args.size == 1) {
+              Hasher.hash(args.head, superSecretKey).flatMap(h => printLine(h))
+            }
+    // When we've been passed 2 args, verify it.
+    _    <- ZIO.when(args.size == 2) {
+              ZIO.ifM(Hasher.validate(args.head, superSecretKey, args.last))(
+                onTrue = printLine("valid"),
+                onFalse = printLine("invalid")
+              )
+            }
   } yield ExitCode.success
 
+  // We call .orDie here to give up, instead of having an error channel,
+  // because if we can't construct our dependencies, our app isn't going to
+  // work anyway.
   val appLayer: ZLayer[Any, Nothing, Hasher] = {
     (HashHelper.specForKey512(
       superSecretKey
